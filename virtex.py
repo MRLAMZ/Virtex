46c41ac56c680539b96f86e336097d5472f78705
FaaL TV <67135772+Kingtebe@users.noreply.github.com>
2021-03-02 16:07:50 +0700

    Add files via upload

---
+++ virtex.py
@@ -0,0 +1,186 @@
+# Awokawokawk Kontol Mau Rikod Ya Anjing ? Kamvret
+# Update : 03-03-2021 14-25
+# Done   : 03-03-2021
+# Only Py3
+
+import os
+import sys
+import time as waktu
+
+M = '\x1b[1;91m'
+K = '\x1b[1;93m'
+H = '\x1b[1;92m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+C = '\x1b[1;96m'
+D = '\x1b[1;90m'
+P = '\x1b[1;97m'
+BB = '\x1b[101m'
+BS = '\033[0m'
+
+'''
+       Author : Kingtebe
+       Youtub : FaaL TV
+       Github : https://github.com/Kingtebe
+
+# Mau Apa Kontol ?
+# Mau Ganti Author ? Ganti Logo ?
+# Gak Guna Anjing, Bikin Sendiri Napa !
+                     © Copyright Kingtebe 2021
+'''
+
+try:
+	import requests
+except ModuleNotFoundError:
+	exit(f'\n {C}[{U}!{C}] {P}Install dulu module requests nya\n     {P} Ketik : {K}pip install requests \n')
+
+class Exit(object):
+	def __init__(self,string):
+		for coli in string + '\n':
+			sys.stdout.write(str(coli));sys.stdout.flush()
+			waktu.sleep(0.001)
+
+class Virtex:
+	def __init__(self):
+		self.x = requests.Session()
+		self.meki1 = 'https://sfile.mobi/downIoad/651791/2/583b246eee7721be65126c591501ce5a/v1.txt&is=513c84532dffe77083b53193d9c08f3e'
+		self.meki2 = 'https://sfile.mobi/downIoad/651804/2/583b246eee7721be65126c591501ce5a/v2.txt&is=15ac9156d81192d86d8c6808281a4704'
+		self.meki3 = 'https://sfile.mobi/downIoad/651806/2/583b246eee7721be65126c591501ce5a/v3.txt&is=a2c166153727a162391f43a8f2814ad8'
+		self.meki4 = 'https://sfile.mobi/downIoad/651810/2/583b246eee7721be65126c591501ce5a/v4.txt&is=a2c166153727a162391f43a8f2814ad8'
+		self.meki5 = 'https://sfile.mobi/downIoad/651809/2/583b246eee7721be65126c591501ce5a/virtex5-.txt&is=a2c166153727a162391f43a8f2814ad8'
+		self.meki6 = 'https://sfile.mobi/downIoad/651811/2/583b246eee7721be65126c591501ce5a/6.txt&is=a2c166153727a162391f43a8f2814ad8'
+		self.meki7 = 'https://sfile.mobi/downIoad/651813/2/583b246eee7721be65126c591501ce5a/v7.txt&is=d15bcb8d75856810af98150e3d262970'
+		self.meki8 = 'https://sfile.mobi/downIoad/651815/2/583b246eee7721be65126c591501ce5a/virtex-8-.txt&is=917825826fd6afa61ebf006a5561cf14'
+		self.meki9 = 'https://sfile.mobi/downIoad/651819/2/583b246eee7721be65126c591501ce5a/v9.txt&is=917825826fd6afa61ebf006a5561cf14'
+		self.puki1 = 'https://sfile.mobi/downIoad/651821/2/583b246eee7721be65126c591501ce5a/v10.txt&is=2079f53aaf2e453b1ec5a2e59b6d10cc'
+		self.puki2 = 'https://sfile.mobi/downIoad/651823/2/583b246eee7721be65126c591501ce5a/v11.txt&is=2079f53aaf2e453b1ec5a2e59b6d10cc'
+		self.meki3 = 'https://sfile.mobi/downIoad/651824/2/583b246eee7721be65126c591501ce5a/v12.txt&is=8ab62ed999f9f3c2b70dbe72131d2887'
+		self.puki4 = 'https://sfile.mobi/downIoad/651827/2/583b246eee7721be65126c591501ce5a/v13.txt&is=99c7753ea34b6e87c8d5fe346c2501ba'
+		self.puki5 = 'https://sfile.mobi/downIoad/651829/2/583b246eee7721be65126c591501ce5a/v14.txt&is=99c7753ea34b6e87c8d5fe346c2501ba'
+		self.puki6 = 'https://sfile.mobi/downIoad/651899/2/583b246eee7721be65126c591501ce5a/virus-mematikan-part-2.txt&is=d45813f38ea3ed9dca4ac1010c926a13'
+		self.Main()
+
+	def Main(self):
+		try:
+			__import__('os').system('clear')
+			Exit(f'\n{P}  _______\n{P} |       | {C}◈ {P}Author{M}: {H}Kingtebe\n{P} |  {U}[•]  {P}| {C}◈ {P}Github{M}: {H}github.com/Kingtebe\n{P} |_______| {C}◈ {P}Script{M}: {H}DOWNLOADER VIRTEX\n {K}----------------------------------------\n {C}[{H}01{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}1\n {C}[{H}02{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}2\n {C}[{H}03{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}3\n {C}[{H}04{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}4\n {C}[{H}05{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}5\n {C}[{H}06{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}6\n {C}[{H}07{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}7\n {C}[{H}08{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}8\n {C}[{H}09{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}9\n {C}[{H}10{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}10\n {C}[{H}11{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}11\n {C}[{H}12{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}12\n {C}[{H}13{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}13\n {C}[{H}14{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}14\n {C}[{H}15{C}] {P}VIRTEX DOWNLOADER WHATSAPP {K}V{P}.{K}15\n {C}[{K}CR{C}] {P}CONTACT / REQUESTS\n {C}[{K}ET{C}] {P}EXIT TOOLS\n {K}----------------------------------------')
+			pil=input(f' {C}[{M}??{C}] {P}Choose{M}: {H} ');print(f' {K}----------------------------------------')
+			if pil in ('1','01'):
+				try:
+					self.api = self.x.get(self.meki1)
+					with open('/sdcard/Virtex1.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex1.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('2','02'):
+				try:
+					self.api = self.x.get(self.meki2)
+					with open('/sdcard/Virtex2.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex2.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('3','03'):
+				try:
+					self.api = self.x.get(self.meki3)
+					with open('/sdcard/Virtex3.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex3.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('4','04'):
+				try:
+					self.api = self.x.get(self.meki4)
+					with open('/sdcard/Virtex4.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex4.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('5','05'):
+				try:
+					self.api = self.x.get(self.meki5)
+					with open('/sdcard/Virtex5.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex5.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('6','06'):
+				try:
+					self.api = self.x.get(self.meki6)
+					with open('/sdcard/Virtex6.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex6.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('7','07'):
+				try:
+					self.api = self.x.get(self.meki7)
+					with open('/sdcard/Virtex7.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex7.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('8','08'):
+				try:
+					self.api = self.x.get(self.meki8)
+					with open('/sdcard/Virtex8.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex8.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('9','09'):
+				try:
+					self.api = self.x.get(self.meki9)
+					with open('/sdcard/Virtex9.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex9.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('10','10'):
+				try:
+					self.api = self.x.get(self.puki1)
+					with open('/sdcard/Virtex10.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex10.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('11','11'):
+				try:
+					self.api = self.x.get(self.puki2)
+					with open('/sdcard/Virtex11.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex11.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('12','12'):
+				try:
+					self.api = self.x.get(self.puki3)
+					with open('/sdcard/Virtex12.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex12.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('13','13'):
+				try:
+					self.api = self.x.get(self.puki4)
+					with open('/sdcard/Virtex13.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex13.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('14','14'):
+				try:
+					self.api = self.x.get(self.puki5)
+					with open('/sdcard/Virtex14.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex14.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('15','15'):
+				try:
+					self.api = self.x.get(self.puki6)
+					with open('/sdcard/Virtex15.txt', 'wb') as meki:
+						meki.write(self.api.content);waktu.sleep(1);Exit(f'\n {C}[{U}••{C}] {P}Sedang Mendownload Virtex {M}...');waktu.sleep(6);Exit(f' {C}[{U}••{C}] {P}Virtex Berhasil Didownload ');waktu.sleep(2);print(f' {C}[{U}••{C}] {P}File saved to »» {H}/sdcard/Virtex15.txt\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('CR','cr'):
+				try:
+					exit(f'\n {C}[{U}••{C}] {P}AwokAwokAwok Ngentod\n {C}[{U}••{C}] {P}Contact Me{M}: {H}wa.me/628212190xxxx\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+			elif pil in ('ET','et'):
+				try:
+					exit(f'\n {U}[ {P}Thank you sudah pakai tools ini {U}]\n')
+				except requests.exceptions.ConnectionError:exit(f'\n {U}[ {P}Warning ! Koneksi Internet Lu Jelek Nyet {U}]\n')
+				except KeyboardInterrupt:exit()
+		except EOFError:exit()
+
+if __name__=='__main__':
+     Virtex()
